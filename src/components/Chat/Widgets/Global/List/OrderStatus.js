b1f2890564d6c686ced65bba11a4d33b203f578b
Remon Atef <vanromany@yahoo.com>
2022-01-13 21:15:53 +0200

    Completed Order Status Widget

--- src/components/Chat/Widgets/Global/List/OrderStatus.js
+++ src/components/Chat/Widgets/Global/List/OrderStatus.js
@@ -19,16 +19,47 @@ const Fun = ({
 
   const barsContent = [];
 
+  const bodyContent = [];
+
   for (let bar of payloadData.body) {
     if (bar.showBar) {
       barsContent.push(
-        <div className={`bar ${bar.done ? "bar-done" : "bar-none"}`}>
+        <div
+          className={`bar ${bar.done ? "bar-done" : "bar-none"}`}
+          key={bar.title.text}
+        >
           <div className="bar-bullet"></div>
           <div className="bar-line"></div>
           <span className="bar-title">{bar.title.text.replace("|", "")}</span>
         </div>
       );
     }
+
+    const bodyListContent = [];
+
+    for (let body of bar.body) {
+      bodyListContent.push(
+        <div className="list-container" key={body.title + body.desc}>
+          {body.title && <div className="body-title">{body.title}</div>}
+          {body.desc && <div className="body-desc">{body.desc}</div>}
+        </div>
+      );
+    }
+
+    bodyContent.push(
+      <div className="body-container" key={bar.title.text}>
+        <div className="status-title-container">
+          {bar.title.text && (
+            <div className="status-title">{bar.title.text}</div>
+          )}
+          {bar.title.desc && (
+            <div className="status-desc">{bar.title.desc}</div>
+          )}
+        </div>
+
+        <div className="body-list-container">{bodyListContent}</div>
+      </div>
+    );
   }
 
   const data = (
@@ -43,6 +74,8 @@ const Fun = ({
         </div>
         <div className="bars">{barsContent}</div>
       </div>
+
+      {show && bodyContent}
     </div>
   );
 
