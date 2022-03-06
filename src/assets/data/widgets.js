dadc979b1bada9176d878f431dfce8a6d4e42254
Remon Atef <vanromany@yahoo.com>
2022-03-06 20:47:45 +0200

    Added Colors Picker For Gradient

---
+++ src/assets/data/widgets.js
@@ -0,0 +1,118 @@
+export const widgets = [
+  {
+    label: "Size",
+    value: "size",
+  },
+  {
+    label: "Type Widget",
+    value: "typeWidget",
+  },
+  {
+    label: "Brand",
+    value: "brand",
+  },
+  {
+    label: "Cart",
+    value: "cart",
+  },
+  {
+    label: "Payment",
+    value: "payment",
+  },
+  {
+    label: "Rate",
+    value: "rate",
+  },
+  {
+    label: "Reason",
+    value: "reason",
+  },
+  {
+    label: "Product List",
+    value: "productList",
+  },
+  {
+    label: "Return Template",
+    value: "returnTemplate",
+  },
+  {
+    label: "Return Confirm",
+    value: "returnConfirm",
+  },
+  {
+    label: "Feedback",
+    value: "feedback",
+  },
+  {
+    label: "OTP",
+    value: "otp",
+  },
+  {
+    label: "Location",
+    value: "location",
+  },
+  {
+    label: "User Location",
+    value: "userLocation",
+  },
+  {
+    label: "Address Info",
+    value: "addressInfo",
+  },
+  {
+    label: "City Select",
+    value: "citySelect",
+  },
+  {
+    label: "Personal Info",
+    value: "personalInfo",
+  },
+  {
+    label: "Maps",
+    value: "maps",
+  },
+  {
+    label: "Personal Info Outline",
+    value: "personalInfoOutline",
+  },
+  {
+    label: "Address Info Outline",
+    value: "addressInfoOutline",
+  },
+  {
+    label: "City Select Outline",
+    value: "citySelectOutline",
+  },
+  {
+    label: "Location Outline",
+    value: "locationOutline",
+  },
+  {
+    label: "General",
+    value: "general",
+  },
+  {
+    label: "Order Status",
+    value: "OrderStatus",
+  },
+  {
+    label: "Order Status Map Widget",
+    value: "OrderStatusMapWidget",
+  },
+  {
+    label: "Article",
+    value: "article",
+  },
+  {
+    label: "Video",
+    value: "video",
+  },
+  {
+    label: "Products Slider",
+    value: "productsSlider",
+  },
+  {
+    label: "Date Picker",
+    value: "dateWidget",
+  },
+];
--- src/components/TestWidgets/Index.jsx
+++ src/components/TestWidgets/Index.jsx
@@ -1,124 +1,7 @@
 import React from "react";
 import "./../../assets/scss/TestWidgets.scss";
-
-const widgets = [
-  {
-    label: "Size",
-    value: "size",
-  },
-  {
-    label: "Type Widget",
-    value: "typeWidget",
-  },
-  {
-    label: "Brand",
-    value: "brand",
-  },
-  {
-    label: "Cart",
-    value: "cart",
-  },
-  {
-    label: "Payment",
-    value: "payment",
-  },
-  {
-    label: "Rate",
-    value: "rate",
-  },
-  {
-    label: "Reason",
-    value: "reason",
-  },
-  {
-    label: "Product List",
-    value: "productList",
-  },
-  {
-    label: "Return Template",
-    value: "returnTemplate",
-  },
-  {
-    label: "Return Confirm",
-    value: "returnConfirm",
-  },
-  {
-    label: "Feedback",
-    value: "feedback",
-  },
-  {
-    label: "OTP",
-    value: "otp",
-  },
-  {
-    label: "Location",
-    value: "location",
-  },
-  {
-    label: "User Location",
-    value: "userLocation",
-  },
-  {
-    label: "Address Info",
-    value: "addressInfo",
-  },
-  {
-    label: "City Select",
-    value: "citySelect",
-  },
-  {
-    label: "Personal Info",
-    value: "personalInfo",
-  },
-  {
-    label: "Maps",
-    value: "maps",
-  },
-  {
-    label: "Personal Info Outline",
-    value: "personalInfoOutline",
-  },
-  {
-    label: "Address Info Outline",
-    value: "addressInfoOutline",
-  },
-  {
-    label: "City Select Outline",
-    value: "citySelectOutline",
-  },
-  {
-    label: "Location Outline",
-    value: "locationOutline",
-  },
-  {
-    label: "General",
-    value: "general",
-  },
-  {
-    label: "Order Status",
-    value: "OrderStatus",
-  },
-  {
-    label: "Order Status Map Widget",
-    value: "OrderStatusMapWidget",
-  },
-  {
-    label: "Article",
-    value: "article",
-  },
-  {
-    label: "Video",
-    value: "video",
-  },
-  {
-    label: "Products Slider",
-    value: "productsSlider",
-  },
-  {
-    label: "Date Picker",
-    value: "dateWidget",
-  },
-];
+import { widgets } from "../../assets/data/widgets";
+import ColorsPicker from "./ColorsPicker";
 
 const TestWidgets = ({
   TEST_MODE,
@@ -128,7 +11,7 @@ const TestWidgets = ({
 }) => {
   return (
     <div id="TestWidgets">
-      <h1>Test Widgets</h1>
+      <h1>Customize Chat</h1>
       <form>
         <div>
           <label htmlFor="mode">Select Mode</label>
@@ -158,6 +41,10 @@ const TestWidgets = ({
             ))}
           </select>
         </div>
+
+        <h3>Select Colors</h3>
+
+        <ColorsPicker />
       </form>
     </div>
   );
