0bfa071563a2ab1b5b6e94d32a46cabfdd4717ab
Remon Atef <vanromany@yahoo.com>
2022-03-14 17:15:40 +0200

    Fix Header

--- test-library/src/App.js
+++ test-library/src/App.js
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 import Chat from 'rasa-chat-library'
 import 'rasa-chat-library/dist/style.css'
 
-const DEFAULT_TEST_MODE = window.localStorage.getItem('TEST_MODE') || 1
-const DEFAULT_SAMPLE_SCHEMA =
-  window.localStorage.getItem('sampleSchema') || 'rate'
-
 function App() {
-  console.log(DEFAULT_SAMPLE_SCHEMA)
-  const [TEST_MODE, setTestMode] = useState(DEFAULT_TEST_MODE)
-  const [sampleSchema, setSampleSchema] = useState(DEFAULT_SAMPLE_SCHEMA)
-
-  useEffect(() => {
-    window.localStorage.setItem('TEST_MODE', TEST_MODE)
-    window.localStorage.setItem('sampleSchema', sampleSchema)
-  }, [TEST_MODE, sampleSchema])
-
   return (
     <div className='App'>
       <Chat
@@ -30,10 +17,6 @@ function App() {
         botAvatar={'N'}
         socketPath={'/socket.io/'}
         customData={{ language: 'en' }}
-        TEST_MODE={TEST_MODE}
-        sampleSchema={sampleSchema}
-        setSampleSchema={setSampleSchema}
-        setTestMode={setTestMode}
       />
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
