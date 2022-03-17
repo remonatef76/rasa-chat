deeb51ae7ce015c3c505e16aa9d610147f866dd8
Remon Atef <vanromany@yahoo.com>
2022-03-17 14:08:12 +0200

    Updates

---
+++ precache-manifest.f4f2bce93d32b9643cde60cad8350816.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffaa989ad50e7258bf154ef335e083f6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d8e15caff1eb6dd87cde",
+    "url": "./static/css/2.245fb51a.chunk.css"
+  },
+  {
+    "revision": "598fa5aae0b92e49ba86",
+    "url": "./static/css/main.4c0f4f7c.chunk.css"
+  },
+  {
+    "revision": "d8e15caff1eb6dd87cde",
+    "url": "./static/js/2.3566c542.chunk.js"
+  },
+  {
+    "revision": "bd9baf8322ca964cfc3ba16aa44416c7",
+    "url": "./static/js/2.3566c542.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "598fa5aae0b92e49ba86",
+    "url": "./static/js/main.1f796250.chunk.js"
+  },
+  {
+    "revision": "f7f1accd5a0cf3abfd31960d7a3872c5",
+    "url": "./static/js/main.1f796250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3719617b57db1f2cc485",
+    "url": "./static/js/runtime-main.a8767dee.js"
+  }
+]);
